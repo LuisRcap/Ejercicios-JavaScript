d1458185aa25912da111f618f6bf3230e2d7cacf
LuisRcap <luisdany7773@gmail.com>
2020-09-01 15:59:47 -0500

    Constantes

---
+++ hola-mundo/js/03-let-var.js
@@ -0,0 +1,26 @@
+'use strict'
+//PRUEBAS DE LET Y VAR
+
+//PRUEBA CON VAR
+var numero = 40;
+console.log(numero);
+
+if(true)
+{
+    numero = 50;
+    console.log(numero);
+}
+
+console.log(numero);
+
+//PRUEBA CON LET
+let texto = "Curso JS";
+console.log(texto);
+
+if(true)
+{
+    let texto = "Curso Laravel 5";
+    console.log(texto);
+}
+
+console.log(texto);
---
+++ hola-mundo/js/04-constantes.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//constantes
+//Como una variable pero su valor no puede cambiar
+
+var web = "https://www.github.com/LuisRcap";
+const ip = "192.88.0.12";
+
+web = "https://www.github.com/LRcap";
+
+console.log(web, ip);
