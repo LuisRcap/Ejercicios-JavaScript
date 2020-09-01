856645c2deb051590dab8a7cdab19113e8f29ccc
LuisRcap <luisdany7773@gmail.com>
2020-09-01 15:41:08 -0500

    Variables y comentarios

---
+++ hola-mundo/js/02-variables.js
@@ -0,0 +1,14 @@
+//VARIABLES
+//UNA VARIABLE ES UN CONTENEDOR DE INFORMACIÓN
+
+var pais = "México";
+var continente = "America";
+var antiguedad = 2020;
+var pais_continente = pais + ' ' + continente;
+
+pais = "España";
+continente = "Europa";
+
+console.log(pais, " ", continente, antiguedad);
+
+alert(pais_continente);
