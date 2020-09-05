abe4abe0778e7c77e874661c38505389c7aef385
LuisRcap <luisdany7773@gmail.com>
2020-09-05 14:37:54 -0500

    Bucle while y do while

--- hola-mundo/js/08-bucle-for.js
+++ hola-mundo/js/08-bucle-for.js
@@ -7,5 +7,5 @@ var numero = 100;
 for(let i = 0; i <= numero; i++)
 {
     console.log("Vamos por el numero " + i);
-    debugger;
+    //debugger;
 }
---
+++ hola-mundo/js/09-while.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//WHILE
+var year = 2018;
+
+while(year != 1990)
+{
+    console.log("Estamos en el año: " + year);
+    year--;
+}
+
+//DO WHILE
+var years = 30;
+
+do
+{
+    alert("Solo cuando sea diferente a 20");
+    years--;
+}while(years > 25)
