9409e74c6f4913c36f81f3acd50047e4dfa02a3e
LuisRcap <luisdany7773@gmail.com>
2020-09-05 11:54:59 -0500

    Operadores y tipos de datos

---
+++ hola-mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//OPERADORES
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("resultado de la operación es: " + operacion);
+
+//TIPOS DE DATOS
+var numero_entero = 44;
+var cadena_texto = "Hola qué tal";
+var verdadero_falso = false;
+
+var numero_falso = "33.4";
+
+console.log(String(numero_entero) + 4);
