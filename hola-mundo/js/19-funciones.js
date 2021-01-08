00f37511b871b143aaf841a58b4fc20057ab19d6
LuisRcap <luisdany7773@gmail.com>
2021-01-08 14:35:48 -0600

    Ejercicio 19 agregado

---
+++ hola-mundo/js/19-funciones.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//Funciones
+//Una función es una agrupación reutilizable de un conjunto de instrucciones
+
+function Calculadora()
+{
+    //Conjunto de instrucciones a ejecutar
+    console.log("Hola, soy la calculadora");
+    console.log("Si, soy yo");
+    //return "Hola, soy la calculadora";
+}
+
+// Invocar o llamar a la fucnión
+var resultado = Calculadora();
+
+console.log(resultado);
