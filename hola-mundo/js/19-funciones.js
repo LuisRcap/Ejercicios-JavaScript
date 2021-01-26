3bcfbbf5177993af59e6f16e711192d28cc14f79
LuisRcap <luisdany7773@gmail.com>
2021-01-26 17:46:55 -0600

    Funciones dentro de otras

--- hola-mundo/js/19-funciones.js
+++ hola-mundo/js/19-funciones.js
@@ -3,30 +3,36 @@
 //Funciones
 //Una función es una agrupación reutilizable de un conjunto de instrucciones
 
+function porConsola(numero1, numero2)
+{
+    console.log("Suma: " + (numero1 + numero2));
+    console.log("Resta: " + (numero1 - numero2));
+    console.log("Multiplicación: "+ (numero1 * numero2));
+    console.log("División: " + (numero1 / numero2));
+    console.log("*************************************");
+}
+
+function porPantalla(numero1, numero2)
+{
+    document.write("Suma: " + (numero1 + numero2) + "<br/>");
+    document.write("Resta: " + (numero1 - numero2)+ "<br/>");
+    document.write("Multiplicación: "+ (numero1 * numero2)+ "<br/>");
+    document.write("División: " + (numero1 / numero2)+ "<br/>");
+    document.write("*************************************"+ "<br/>");
+}
+
 function Calculadora(numero1, numero2, mostrar = false)
 {
-    //Conjunto de instrucciones a ejecutar
     if(mostrar == false)
     {
-        console.log("Suma: " + (numero1 + numero2));
-        console.log("Resta: " + (numero1 - numero2));
-        console.log("Multiplicación: "+ (numero1 * numero2));
-        console.log("División: " + (numero1 / numero2));
-        console.log(mostrar);
-        console.log("*************************************");
+        porConsola(numero1, numero2);
     }
     else
     {
-        document.write("Suma: " + (numero1 + numero2) + "<br/>");
-        document.write("Resta: " + (numero1 - numero2)+ "<br/>");
-        document.write("Multiplicación: "+ (numero1 * numero2)+ "<br/>");
-        document.write("División: " + (numero1 / numero2)+ "<br/>");
-        document.write(mostrar + "</br>");
-        document.write("*************************************");
+        porPantalla(numero1, numero2);
     }
 
-    //return "Hola, soy la calculadora";
-    
+    return true;
 }
 
 // Invocar o llamar a la fucnión
@@ -34,6 +40,10 @@ function Calculadora(numero1, numero2, mostrar = false)
 Calculadora(1, 4);
 
 Calculadora(2, 5, true);
+
+Calculadora(4, 5, true);
+
+Calculadora(4, 8);
 
 /*
 for(var i = 1; i <= 10; i++)
