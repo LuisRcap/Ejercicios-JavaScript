b9949b7a9a0dc1a828e7437203d7f87b986cef7f
LuisRcap <luisdany7773@gmail.com>
2021-01-26 17:40:40 -0600

    Uso de funciones en ejercicio 19

--- hola-mundo/js/19-funciones.js
+++ hola-mundo/js/19-funciones.js
@@ -3,21 +3,43 @@
 //Funciones
 //Una función es una agrupación reutilizable de un conjunto de instrucciones
 
-function Calculadora(numero1, numero2)
+function Calculadora(numero1, numero2, mostrar = false)
 {
     //Conjunto de instrucciones a ejecutar
-    console.log("Suma: " + (numero1 + numero2));
-    console.log("Resta: " + (numero1 - numero2));
-    console.log("Multiplicación: "+ (numero1 * numero2));
-    console.log("División: " + (numero1 / numero2));
-    console.log("*************************************");
-    return "Hola, soy la calculadora";
+    if(mostrar == false)
+    {
+        console.log("Suma: " + (numero1 + numero2));
+        console.log("Resta: " + (numero1 - numero2));
+        console.log("Multiplicación: "+ (numero1 * numero2));
+        console.log("División: " + (numero1 / numero2));
+        console.log(mostrar);
+        console.log("*************************************");
+    }
+    else
+    {
+        document.write("Suma: " + (numero1 + numero2) + "<br/>");
+        document.write("Resta: " + (numero1 - numero2)+ "<br/>");
+        document.write("Multiplicación: "+ (numero1 * numero2)+ "<br/>");
+        document.write("División: " + (numero1 / numero2)+ "<br/>");
+        document.write(mostrar + "</br>");
+        document.write("*************************************");
+    }
+
+    //return "Hola, soy la calculadora";
+    
 }
 
 // Invocar o llamar a la fucnión
 
+Calculadora(1, 4);
+
+Calculadora(2, 5, true);
+
+/*
 for(var i = 1; i <= 10; i++)
 {
     console.log(i);
     Calculadora(i,8)
 }
+*/
+
