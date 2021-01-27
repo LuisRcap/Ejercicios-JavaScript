442bf4c8c933b221889dc313415f3fa0bd5c121f
LuisRcap <luisdany7773@gmail.com>
2021-01-26 19:35:20 -0600

    Métodos de búsqueda en Strings

--- hola-mundo/js/23-funciones-texto.js
+++ hola-mundo/js/23-funciones-texto.js
@@ -2,25 +2,20 @@
 
 // Transformación de textos
 var numero = 444;
-var texto1 = "Binvenido al curso de JavaScript de Víctor Robles";
+var texto1 = "Bienvenido al curso de JavaScript curso de Víctor Robles";
 var texto2 = "Es muy buen curso";
 
-var dato = numero.toString();
-dato = texto1.toLowerCase();
-dato = texto2.toUpperCase();
-//console.log(dato);
 
-// Calcular longitud
+//var busqueda = texto1.lastIndexOf("curso");       //Localiza la última coincidencia
+//var busqueda = texto1.indexOf("curso");           //Localiza en el texto
+//var busqueda = texto1.search("curso");            //Localiza en el texto
+//var busqueda = texto1.match("curso");             //Devuelve un arreglo con varios detalles
+//var busqueda = texto1.match(/curso/g);            //Localiza cúantas veces encontró la coincidencia
+//var busqueda = texto1.substr(14, 5);              //Saca una subcadena
+//var busqueda = texto1.charAt(44);                 //Localiza un caracter en el texto
+//var busqueda = texto1.startsWith("Víctor");       //Busca al inicio del texto y devuelve un bool
+//var busqueda = texto1.endsWith("Víctor Robles");  //Busca al final del texto y devuelve un bool
+var busqueda = texto1.includes("JavaScript");       //Indica si el texto está o no en el texto de busqueda
 
-var nombre = "Luis Rico";
-    nombre = ["hoa", "hola"];
+console.log(busqueda);
 
-//console.log(nombre.length);
-
-// Concatenar = Unir textos
-
-//var textoTotal = texto1 + " " + texto2;
-
-var textoTotal = texto1.concat(" "+ texto2);
-
-console.log(textoTotal);
