a9e9724bee6c0baf1ffe10785e5da721661147ea
LuisRcap <luisdany7773@gmail.com>
2021-01-28 13:47:01 -0600

    Arrays avanzados

--- hola-mundo/js/25-arrays.js
+++ hola-mundo/js/25-arrays.js
@@ -6,6 +6,24 @@ var nombre = "Luis Rico";
 
 var nombres = ["Luis Rico", "Juan Lopez", "Manolo García", "José Martín", 52, true];
 
-var lenguajes = new Array("PHP", "JS", "GO", "Java");
+var lenguajes = new Array("PHP", "JS", "GO", "Java", "C#", "C", "Pascal");
 
-console.log(nombres[0]);
+/*
+var elemento = parseInt(prompt("¿Qué elemento del array quieres?", 0));
+if(elemento >= nombres.length)
+{
+    alert("Introduce el número correcto menor a " + nombres.length);
+}
+else
+{
+    alert("El usuario seleccionado es: " + nombres[elemento]);
+}*/
+
+document.write("<h1>Lenguajes de programación del 2018</h1>");
+document.write("<ul>");
+for(var i = 0; i < lenguajes.length; i++)
+{   
+    document.write("<li>" + lenguajes[i] + "</li>");
+}
+
+document.write("</ul>");
