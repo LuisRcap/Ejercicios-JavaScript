6a61a9f1d1a97b670b5912014907c59a5c1bb30f
LuisRcap <luisdany7773@gmail.com>
2021-01-28 14:45:20 -0600

    Operaciones con arreglos

--- hola-mundo/js/26-arrays-multi.js
+++ hola-mundo/js/26-arrays-multi.js
@@ -5,5 +5,29 @@ var peliculas = ['La verdad duele', 'La vida es bella', 'Gran Torino'];
 
 var cine = [categorias, peliculas];
 
-console.log(cine[0][1]);
-console.log(cine[1][2]);
+//console.log(cine[0][1]);
+//console.log(cine[1][2]);
+
+/*var elemento = prompt("Introduce tu película:");
+
+while(elemento != "ACABAR")
+{
+    peliculas.push(elemento);
+    elemento = prompt("Introduce tu película:");
+}
+
+//peliculas.pop();                                    //Quita un elemento de un array
+//peliculas[0] = undefined;                           //Pone el elemento como indefinido
+*/
+
+
+var indice = peliculas.indexOf('Gran Torino'); 
+
+if(indice > -1)
+{
+    peliculas.splice(indice, 1);                    //Borra elementos de un arreglo y de ser necesario inserta otros nuevos además de regresar los borrados
+}
+
+var peliculas_string = peliculas.join();            //Convierte el array en un string separado por comas
+
+console.log(peliculas_string);
