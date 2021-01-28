969e7823b81c824fc80172f3ace8b2401b66b75d
LuisRcap <luisdany7773@gmail.com>
2021-01-28 13:20:22 -0600

    Arrays básicos

---
+++ hola-mundo/js/25-arrays.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Arrays, Arreglos, Matrices
+
+var nombre = "Luis Rico";
+
+var nombres = ["Luis Rico", "Juan Lopez", "Manolo García", "José Martín", 52, true];
+
+var lenguajes = new Array("PHP", "JS", "GO", "Java");
+
+console.log(nombres[0]);
