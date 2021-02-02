7c4e908f18e0ad88b33786b5ff7689b95bd23ab2
LuisRcap <luisdany7773@gmail.com>
2021-02-02 16:33:28 -0600

    Seleccionar y modificar elementos de la página

--- hola-mundo/js/28-dom.js
+++ hola-mundo/js/28-dom.js
@@ -7,6 +7,7 @@ function cambiaColor(color)
     caja.style.background = color;
 }
 
+//Conseguir elementos con un id concreto
 
 //var caja = document.getElementById("micaja");
 var caja = document.querySelector("#micaja");
@@ -16,7 +17,29 @@ caja.innerHTML = "¡TEXTO EN LA CAJA DESDE JS!";
 caja.style.background = "red";
 caja.style.padding = "20px";
 caja.style.color = "white";
-
 caja.className = "hola hola2";
+
+//Conseguir elementos por su etiqueta
+var todosLosDivs = document.getElementsByTagName('div');
+
+var seccion = document.querySelector("#miseccion");
+var hr = document.createElement("hr");
+
+
+for(var valor in todosLosDivs)
+{
+    if(typeof todosLosDivs[valor].textContent == 'string')
+    {
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(todosLosDivs[valor].textContent);
+        parrafo.append(texto);   
+        seccion.append(parrafo);
+    }
+}
+
+seccion.append(hr);
+
+
+//Conseguir elementos por su clase
 
 console.log(caja);
