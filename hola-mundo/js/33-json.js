1468aefc6b128a2c83f3af87b5de148175b319a7
LuisRcap <luisdany7773@gmail.com>
2021-02-05 19:54:34 -0600

    JSON - ¿Cómo se utilizan?

---
+++ hola-mundo/js/33-json.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// JSON - JavaScript Object Notation
+
+window.addEventListener('load', () =>
+{
+    var pelicula = 
+    {
+        titulo: 'Batman vs Superman',
+        year: 2017,
+        pais: 'Estados Unidos'
+    };
+
+    var peliculas =
+    [
+        {titulo: "La verdad duele", year: 2016, pais: "Francia"},
+        pelicula
+    ];
+
+    pelicula.titulo = "Superman Begins";
+
+    var caja_peliculas = document.querySelector("#peliculas");
+    for(var index in peliculas)
+    {
+        var p = document.createElement("p");
+        p.append(peliculas[index].titulo + " - " + peliculas[index].year);
+        caja_peliculas.append(p);
+    }
+});
