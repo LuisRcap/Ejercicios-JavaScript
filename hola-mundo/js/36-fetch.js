ab3d59435b047cae1631789cd0e774f9742a82cc
LuisRcap <luisdany7773@gmail.com>
2021-02-06 18:06:22 -0600

    Crear promesas fetch en JS

--- hola-mundo/js/36-fetch.js
+++ hola-mundo/js/36-fetch.js
@@ -2,24 +2,91 @@
 
 // Fetch (ajax) y peticiones a servicios / apis rest
 var div_usuarios = document.querySelector("#usuarios");
-var usuarios = [];
+var div_profesor = document.querySelector("#profesor");
+var div_janet = document.querySelector("#janet");
 
 //fetch('https://jsonplaceholder.typicode.com/users')           //Otra página para capturar jsons con fetch
-fetch('https://reqres.in/api/users?page=2')
+//fetch('https://reqres.in/api/users?page=2')
+    getUsuarios()
     .then(data => data.json())
     .then(users => 
         {
-            usuarios = users.data;
-            console.log(usuarios);
-
-            usuarios.map((user, i) =>
+            listadoUsuarios(users.data);
+            return getInfo();
+        })
+        .then(data =>
             {
-                let nombre = document.createElement("h3");
-                nombre.innerHTML = i + ". " + user.first_name + " " + user.last_name;
+                div_profesor.innerHTML = data;
 
-                div_usuarios.appendChild(nombre);
-
-                document.querySelector(".loading").style.display = "none";
-                
+                return getJanet();
+            })
+        .then(data => data.json())
+        .then(user => 
+            {
+                mostrarJanet(user.data);
             });
-        });
+        
+
+function getUsuarios()
+{
+    return fetch('https://reqres.in/api/users?page=2');
+}
+
+function getJanet()
+{
+    return fetch('https://reqres.in/api/users/2');
+}
+
+function getInfo()
+{
+    var profesor = 
+    {
+        nombre: 'Luis',
+        apellidos: 'Rico',
+        url: 'https://github.com/LuisRcap'
+    };
+
+    return new Promise((resolve, reject) => 
+    {
+        var profesor_string = "";
+        setTimeout(function()
+        {
+            profesor_string = JSON.stringify(profesor);
+
+            if(typeof profesor_string != 'string' && profesor_string != '')
+                return reject('error');
+            else
+                return resolve(profesor_string);
+        }, 3000);
+
+        
+    });
+}
+
+function listadoUsuarios(usuarios)
+{
+    usuarios.map((user, i) =>
+    {
+        let nombre = document.createElement("h3");
+        nombre.innerHTML = i + ". " + user.first_name + " " + user.last_name;
+
+        div_usuarios.appendChild(nombre);
+
+        document.querySelector(".loading").style.display = "none";
+    });
+}
+
+function mostrarJanet(user)
+{
+    let nombre = document.createElement("h4");
+    let avatarDeFuncion = document.createElement("img");
+
+    nombre.innerHTML = user.first_name + " " + user.last_name;
+    avatarDeFuncion.src = user.avatar;
+    avatarDeFuncion.width = "100";
+
+    div_janet.appendChild(nombre);
+    div_janet.appendChild(avatarDeFuncion);
+
+    document.querySelector("#janet .loading").style.display = "none";
+}
