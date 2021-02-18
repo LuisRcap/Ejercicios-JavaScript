3840a930b042d33bcf677d08582c6787b35a5003
LuisRcap <luisdany7773@gmail.com>
2021-02-18 17:50:34 -0600

    Update: Archivo fechas.js agregado

---
+++ hola-mundo/js/38-fechas.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var fecha = new Date();
+
+var year = fecha.getFullYear();
+var mes = fecha.getMonth();
+var dia = fecha.getDate();
+var hora = fecha.getHours();
+
+var textoHora = 
+`
+    El año es: ${year}
+    El mes es: ${mes+1}
+    El día es: ${dia}
+    La hora es: ${hora}
+`;
+
+console.log(textoHora);
