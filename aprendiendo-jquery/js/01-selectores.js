59eeeeb2c5fc65bc0b1b7b108a0cd183e89a4db7
LuisRcap <luisdany7773@gmail.com>
2021-04-29 19:33:26 -0500

    Selector de ID con JQuery

---
+++ aprendiendo-jquery/js/01-selectores.js
@@ -0,0 +1,12 @@
+'use strict'
+
+$(document).ready(() =>
+{
+    //SELECTOR DE ID
+    $("#rojo").css("background", "red")
+                .css("color", "white");
+    $("#amarillo").css("background", "yellow")
+                .css("color", "green");
+    $("#verde").css("background", "green")
+                .css("color", "white");
+});
