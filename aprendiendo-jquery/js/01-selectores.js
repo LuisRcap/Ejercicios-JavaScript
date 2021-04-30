954b409668d7538824561182eb013f9a7df0571c
LuisRcap <luisdany7773@gmail.com>
2021-04-29 19:49:47 -0500

    Selectores de clases JQuery

--- aprendiendo-jquery/js/01-selectores.js
+++ aprendiendo-jquery/js/01-selectores.js
@@ -9,4 +9,13 @@ $(document).ready(() =>
                 .css("color", "green");
     $("#verde").css("background", "green")
                 .css("color", "white");
+
+    //SELECTORES DE CLASES
+    var miClase = $(".zebra").css("padding", "5px");
+
+    $(".sin_borde").click(function()
+    { 
+        console.log("click dato");
+        $(this).addClass("zebra");        
+    });
 });
