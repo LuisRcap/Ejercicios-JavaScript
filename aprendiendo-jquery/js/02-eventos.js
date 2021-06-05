85f27b6efce6e97a0835ee6691efe8ecea7856f9
LuisRcap <luisdany7773@gmail.com>
2021-06-04 20:05:38 -0500

    JQuery cambio de color con funcion hover

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -3,11 +3,22 @@ $(document).ready(function()
     // MouseOver MouseOut
     var caja = $("#caja");
 
-    caja.mouseover(function () { 
+    /*caja.mouseover(function () { 
         $(this).css("background", "red");
     });
 
     caja.mouseout(function () { 
         $(this).css("background", "green");
-    });
+    });*/
+
+    function cambiaRojo() { 
+        $(this).css("background", "red");
+    }
+
+    function cambiaVerde() { 
+        $(this).css("background", "green");
+    }
+
+    // Hover
+    caja.hover(cambiaRojo, cambiaVerde);
 });
