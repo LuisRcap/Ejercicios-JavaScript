7ade3b0adf4fad97b2fba242134307fe56d35327
LuisRcap <luisdany7773@gmail.com>
2021-06-26 16:43:53 -0500

    JQuery - Get con Ajax

--- aprendiendo-jquery/js/05-ajax.js
+++ aprendiendo-jquery/js/05-ajax.js
@@ -1,5 +1,14 @@
 $(document).ready(() =>
 {
     //Load
-    $('#datos').load("https://reqres.in/");
+    //$('#datos').load("https://reqres.in/");
+
+    // GET y POST JQuery
+    $.get("https://reqres.in/api/users", {page: 2}, function(response)
+    {
+        response.data.forEach((element, index) => 
+        {
+            $('#datos').append("<p>" + element.first_name + " " + element.last_name + "</p>");    
+        });
+    });
 });
