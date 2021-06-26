8942c9a943c3e08364394ae694a5445f69b33303
LuisRcap <luisdany7773@gmail.com>
2021-06-26 17:17:22 -0500

    JQuery - método POST y $.ajax

--- aprendiendo-jquery/js/05-ajax.js
+++ aprendiendo-jquery/js/05-ajax.js
@@ -11,4 +11,51 @@ $(document).ready(() =>
             $('#datos').append("<p>" + element.first_name + " " + element.last_name + "</p>");    
         });
     });
+
+
+    
+
+    $('#formulario').submit(function(e)
+    {
+        e.preventDefault();
+        
+        var usuario = 
+        {
+            name: $('input[name="name"]').val(),
+            web: $('input[name="web"]').val()
+        };
+        
+        /*
+        $.post($(this).attr('action'), usuario, function(response)
+        {
+            console.log(response);
+        }).done(function()
+        {
+            alert("Usuario añadido correctamente");
+        });*/
+
+        $.ajax(
+        {
+            type: "POST",
+            //dataType: "json",                     //Esto no es realmente necesario
+            //contentType: 'application/json',
+            url: $(this).attr("action"),
+            data: usuario,
+            beforeSend: function()
+            {
+                console.log("Enviando usuario...");
+            },
+            success: function(response)
+            {
+                console.log(response);
+            },
+            error: function()
+            {
+                console.log("Ha ocurrido un error");
+            },
+            timeout: 2000
+        });
+
+        return false;
+    });
 });
