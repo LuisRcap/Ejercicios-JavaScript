adf710cb32665e8532a5bf0bb4b59d057e31f8de
LuisRcap <luisdany7773@gmail.com>
2021-07-06 21:58:59 -0500

    jQuery-UI - Animaciones y efectos

--- aprendiendo-jquery/js/06-interface.js
+++ aprendiendo-jquery/js/06-interface.js
@@ -24,5 +24,19 @@ $(document).ready(() =>
         }
     });
 
+    // Efectos
+    $('#mostrar').click(function()
+    {
+        //$('.caja-efectos').fadeToggle();            //Efecto de desvanecido
+        //$('.caja-efectos').toggle('explode');       // Efecto de separación en varios pedazos
+        //$('.caja-efectos').toggle('blind');         // Efecto de cortina
+        //$('.caja-efectos').toggle('slide');         // Efecto de deslizamiento
+        //$('.caja-efectos').toggle('drop');          // Efecto de deslizamiento con degradado
+        //$('.caja-efectos').toggle('fold');          // Efecto de desdoblamiento
+        //$('.caja-efectos').toggle('puff');          // Efecto de aparecer y desaparecer desvanecido
+        //$('.caja-efectos').toggle('scale');         // Efecto de agrandar
+        //$('.caja-efectos').toggle('shake', "slow");         // Efecto de sacudir y aparecer con parametro de velocidad
+        $('.caja-efectos').toggle('shake', 4000);       // Efecto con velocidad en milisegundos
+    });
 
 });
