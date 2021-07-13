c5cf93ca282ba170c67523819ff822d497d0ce7d
LuisRcap <luisdany7773@gmail.com>
2021-07-13 17:08:28 -0500

    TypeScript - let vs var

--- poo-y-typescript/js/03-tipos-de-datos.js
+++ poo-y-typescript/js/03-tipos-de-datos.js
@@ -12,4 +12,13 @@ cualquiera = 77;
 // Array
 var lenguajes = ['PHP', "JS", 'CSS'];
 var years = ['DOCE', 13, 14];
+// let vs var
+var numero1 = 10;
+var numero2 = 12;
+if (numero1 == 10) {
+    var numero1_1 = 44;
+    var numero2 = 55;
+    console.log(numero1_1, numero2);
+}
+console.log(numero1, numero2);
 console.log(cadena, numero, verdadero_falso, lenguajes, years);
--- poo-y-typescript/js/03-tipos-de-datos.ts
+++ poo-y-typescript/js/03-tipos-de-datos.ts
@@ -20,4 +20,17 @@ var lenguajes: Array<string> = ['PHP', "JS", 'CSS'];
 
 let years: any[] = ['DOCE', 13, 14];
 
+// let vs var
+var numero1 = 10
+var numero2 = 12
+
+if(numero1 == 10)
+{
+    let numero1 = 44;
+    var numero2 = 55;
+    console.log(numero1, numero2);
+}
+
+console.log(numero1, numero2);
+
 console.log(cadena, numero, verdadero_falso, lenguajes, years);
