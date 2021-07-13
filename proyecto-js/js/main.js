0fe11ba5060b400564518afa62de3abdc870a316
LuisRcap <luisdany7773@gmail.com>
2021-07-12 23:02:38 -0500

    Proyecto 1 - Reloj dinamico

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -120,9 +120,20 @@ $(document).ready(() =>
         });
     }
 
+    // Acordeon
     if(window.location.href.indexOf('about') > -1)
     {
         $('#acordeon').accordion();
+    }
+
+    // Reloj
+    if(window.location.href.indexOf('reloj') > -1)
+    {
+        setInterval(function()
+        {
+            var reloj = moment().format("hh:mm:ss");
+            $('#reloj').html(reloj);
+        },1000);
     }
 
 });
