cacb9d707a0e8399d6ef867385e0dd6679afa2bc
LuisRcap <luisdany7773@gmail.com>
2021-07-13 16:46:14 -0500

    TypeScripts - Tipos de dato

---
+++ poo-y-typescript/js/03-tipos-de-datos.js
@@ -0,0 +1,13 @@
+// string
+var cadena = "LuisRcap";
+// number
+var numero = 12;
+// booleano
+var verdadero_falso = true;
+// Any
+var cualquiera = 'hola';
+cualquiera = 77;
+// Array
+var lenguajes = ['PHP', "JS", 'CSS'];
+var years = ['DOCE', 13, 14];
+console.log(cadena, numero, verdadero_falso, lenguajes, years);
---
+++ poo-y-typescript/js/03-tipos-de-datos.ts
@@ -0,0 +1,19 @@
+// string
+let cadena: string = "LuisRcap";
+
+// number
+let numero: number = 12;
+
+// booleano
+let verdadero_falso: boolean = true;
+
+// Any
+let cualquiera: any = 'hola';
+cualquiera = 77;
+
+// Array
+var lenguajes: Array<string> = ['PHP', "JS", 'CSS'];
+
+let years: any[] = ['DOCE', 13, 14];
+
+console.log(cadena, numero, verdadero_falso, lenguajes, years);
