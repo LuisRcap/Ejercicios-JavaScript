b9467d74af1608dd961165951f83e9dbf0f3038f
LuisRcap <luisdany7773@gmail.com>
2021-07-21 22:45:50 -0500

    Angular - Componentes

---
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,23 @@
+import{ Component } from '@angular/core';
+
+@Component(
+{
+    selector: 'videojuego',
+    template: `
+        <h2>Componente de videojuegos</h2>
+        <ul>
+            <li>GTA V</li>
+            <li>Prince of Persia</li>
+            <li>Tekken</li>
+            <li>Mario</li>
+        </ul>
+    `
+})
+
+export class VideojuegoComponent
+{
+    constructor()
+    {
+        console.log("Se ha cargado el componente: videojuego.component");
+    }
+}
