1aa2bf19760c8b186bc29c9f0a4474cfbef43ecf
LuisRcap <luisdany7773@gmail.com>
2021-07-21 23:19:51 -0500

    Angular - Componente desde cli

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponent,
-    ZapatillasComponent
+    ZapatillasComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
