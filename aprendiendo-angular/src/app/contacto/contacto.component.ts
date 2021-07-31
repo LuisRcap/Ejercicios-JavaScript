d1ff24c213aaf631e8a65d7f63f45c8c7f1953a4
LuisRcap <luisdany7773@gmail.com>
2021-07-31 17:33:05 -0500

    Angular - Validar formulario

--- aprendiendo-angular/src/app/contacto/contacto.component.ts
+++ aprendiendo-angular/src/app/contacto/contacto.component.ts
@@ -18,10 +18,10 @@ export class ContactoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit()
+  onSubmit(form:any)
   {
-    console.log("Evento submit lanzado");
     console.log(this.usuario);
+    //form.reset();
   }
 
 }
