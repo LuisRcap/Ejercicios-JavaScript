74b3b78238dc0c610fedeec09c9627a4974ebaea
LuisRcap <luisdany7773@gmail.com>
2021-08-06 23:54:26 -0500

    Proyecto - Componentes y rutas

---
+++ proyecto-angular/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { routing, AppRoutingProviders } from './app-routing.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    AppRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
