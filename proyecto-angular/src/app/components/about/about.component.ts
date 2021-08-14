f3d65af9596c4bc291678cf36337fbf276f95229
LuisRcap <luisdany7773@gmail.com>
2021-08-14 17:05:34 -0500

    Estilos en about

--- proyecto-angular/src/app/components/about/about.component.ts
+++ proyecto-angular/src/app/components/about/about.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public subtitle: string;
+  public web: string;
+
+  constructor(){
+    this.title = "Luis Rico";
+    this.subtitle = "Desarrollador web y estudiante";
+    this.web = "www.github.com/LuisRcap";
+  }
 
   ngOnInit(): void {
   }
