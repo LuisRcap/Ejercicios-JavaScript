2e778a526115c03a3fb45d4c800a7d238de250d3
LuisRcap <luisdany7773@gmail.com>
2021-08-23 23:09:57 -0500

    Directivas personalizadas

--- proyecto-angular/src/app/components/contact/contact.component.ts
+++ proyecto-angular/src/app/components/contact/contact.component.ts
@@ -27,7 +27,7 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
     var opcion_clasica = document.querySelector('#texto')?.innerHTML;    
-    alert(this.textos.nativeElement.textContent);
+    //alert(this.textos.nativeElement.textContent);
   }
 
   cargarSlider(){
