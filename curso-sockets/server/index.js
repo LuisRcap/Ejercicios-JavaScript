f398430eaa5c6a7b0ddafcfe749e786c84749dcb
LuisRcap <luisdany7773@gmail.com>
2021-08-25 20:45:07 -0500

    Mensajes en el chat

--- curso-sockets/server/index.js
+++ curso-sockets/server/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
-const { Socket } = require('socket.io');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var io = require('socket.io')(server, {
+    cors: {//Permite el acceso de orígenes mixtos (CORS)
+        origin: '*'
+    }
+});
 
 app.use(express.static('client'));
 
@@ -10,8 +13,18 @@ app.get('/hola-mundo', function(req, res){
     res.status(200).send('Hola mundo desde una ruta');
 });
 
+var messages = [
+    {
+        id: 1,
+        text: "Bienvenido al chat privado de socket.io y NodeJS de LuisRcap...",
+        nickname: "Bot - LuisRcap"
+    }
+];
+
 io.on('connection', function(socket){
     console.log("El cliente con IP " + socket.handshake.address + " se ha conectado...");
+
+    socket.emit('messages', messages);
 });
 
 server.listen(6677, function(){
