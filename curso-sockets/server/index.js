555bef44380d15da91f499158773ca3f9f0cd8a2
LuisRcap <luisdany7773@gmail.com>
2021-08-25 21:02:55 -0500

    Enviar y recibir mensajes

--- curso-sockets/server/index.js
+++ curso-sockets/server/index.js
@@ -25,6 +25,13 @@ io.on('connection', function(socket){
     console.log("El cliente con IP " + socket.handshake.address + " se ha conectado...");
 
     socket.emit('messages', messages);
+
+    socket.on('add-message', function(data){
+        messages.push(data);
+
+        io.sockets.emit('messages', messages);
+    });
+
 });
 
 server.listen(6677, function(){
