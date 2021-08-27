383dfbf81a3884b7c9e424f62346c1c40ac0b0fe
LuisRcap <luisdany7773@gmail.com>
2021-08-26 22:09:54 -0500

    TypeScript - Ejercicio POO

--- curso-typescript/programa.js
+++ curso-typescript/programa.js
@@ -37,3 +37,19 @@ editor.setNombre("Camtasia Studio");
 editor.setVersion(8);
 editor.setTimeLine(4000);
 console.log(editor.getAllData());
+// Lógica del formulario
+var programas = [];
+function guardar() {
+    var nombre = document.getElementById("nombre").value.toString();
+    var programa = new Programa();
+    programa.setNombre(nombre);
+    programa.setVersion(1);
+    programas.push(programa);
+    var list = "";
+    for (let i = 0; i < programas.length; i++) {
+        list = list + "<li>" + programas[i].getNombre() + "</li>";
+    }
+    var listado = document.getElementById("listado");
+    listado.innerHTML = list;
+    document.getElementById("nombre").value = "";
+}
